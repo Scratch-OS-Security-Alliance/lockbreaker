feb951462c68f3dc3c7dbcf28533773a7d4455ee
bambus <132773043+bambus80@users.noreply.github.com>
2024-06-25 10:38:05 +0200

    fix wrong program name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.sb3zip import zipProject, unzipProject, cleanup
 from src.sb3patcher import patchProjectJSON
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='FreezerBurn', description='pastaOS Anticheat Patcher')
+    parser = argparse.ArgumentParser(prog='Lockbreaker', description='Bypass openMX logins')
     parser.add_argument('-F', '--filedir', type=str, required=True)
     parser.add_argument('-T', '--tmpdir', type=str, required=False, default=os.path.join(os.getcwd(), 'tmp'))
     args = parser.parse_args()
